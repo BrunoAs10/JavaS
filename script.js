cbff359b08bee94c39c93466810152ed5fc7d19d
BrunoAs10 <bruno_as10@hotmail.com>
2022-10-11 17:53:59 -0300

    cambio de proyecto

--- script.js
+++ script.js
@@ -1,32 +1,71 @@
-function Producto(nombre, precio, stock){
+function Producto(nombre, precio, stock, cat){
     this.nombre = nombre;
     this.precio =precio;
     this.stock = stock || 0;
+    this.cat = cat
     this.restarStock = function(cantidad){
         this.stock -= cantidad
     }
-}
-
-let productoA = new Producto("Tortillas De Harina de Maiz", 350, 10)
-let productoB = new Producto("Tortillas de Harina de Trigo", 300, 10)
-let productoC = new Producto("Tortillas de Harina de Trigo para Burritos", 300, 10)
-let productoD = new Producto("Nachos de Harina de Maiz", 250, 10)
-let productoE = new Producto("Salsa Tabasco", 750, 10)
-let productoF = new Producto("Salsa Picante Valentina", 1200, 10)
-let productoG = new Producto("Chiles Picantes Varios jalapeños,Habaneros, Arbol", 250, 10)
-let precioTotal = 0
-
-let listaProductos = [productoA, productoB, productoC, productoD, productoE, productoF, productoG]
-
-let listaNombres = []
-
-for(const prod of listaProductos){
-    if(prod.stock > 0){
-        listaNombres.push(prod.nombre)
+    this.sumarStock = function(cantidad){
+        this.stock += cantidad
     }
 }
 
-alert("Estos son nuestros productos: \n - "+  listaNombres.join("\n - "))
+let productoA = new Producto("Tortillas De Harina de Maiz", 350, 10, "Comida")
+let productoB = new Producto("Tortillas de Harina de Trigo", 300, 10, "Comida")
+let productoC = new Producto("Tortillas de Harina de Trigo para Burritos", 300, 10, "Comida")
+let productoD = new Producto("Nachos de Harina de Maiz", 250, 10, "Comida")
+let productoE = new Producto("Salsa Tabasco", 750, 10, "Salsa")
+let productoF = new Producto("Salsa Picante Valentina", 1200, 10, "Salsa")
+let productoG = new Producto("Chiles Picantes Varios jalapeños,Habaneros, Arbol", 250, 10, "Comida")
+let productoH = new Producto("Tequila", 2000, 10, "Bebida")
+let productoI = new Producto("Mezcal", 2500, 10, "Bebida")
+
+let listaProductos = [productoA, productoB, productoC, productoD, productoE, productoF, productoG, productoH, productoI]
+
+let listaProductosConStock = listaProductos.filter((prod) => prod.stock > 0)
+
+let listaNombres = listaProductosConStock.map((prod) => prod.nombre)
+
+let precioTotal = 0
+
+let catalogo = document.getElementById("catalogo")
+
+function render(lista) {
+    //catalogo.innerHTML = ""
+
+    for(const prod of lista){
+
+        let card = document.createElement("div")
+        
+        card.className = "card"
+
+        card.innerHTML = `<h2>${prod.nombre}<h2><p>$${prod.precio}</p>`
+
+        catalogo.append(card)
+    }
+}
+render(listaProductosConStock)
+
+let categoria = prompt("Ingrese la categoria que quiere ver: \n1- Bebida\n2 - Comida\n3 - Salsa")
+
+let filtroActual = listaProductos.filter((prod)=>prod.cat == categoria)
+
+
+
+render(filtroActual)
+
+
+
+
+
+/*for(const prod of listaProductos){
+    if(prod.stock > 0){
+        listaNombres.push(prod.nombre)
+    }
+} */
+
+/*alert("Estos son nuestros productos: \n - "+  listaNombres.join("\n - "))
 
 function calculoPrecio(cantidad, precio){
     precioTotal += (cantidad * precio)
@@ -41,7 +80,7 @@ function CalculoStock(cantidad, stock, precio){
     }
 }
 
-let opcion = prompt("Ingrese que es lo que desea comprar: \n1 - Tortillas de Harina de Maíz\n2 - Tortillas de Harina de Trigo\n3 - Tortillas de Harina de Trigo para Burritos\n4 - Nachos de Harina de Maiz\n5 - Salsa Tabasco\n6 - Salsa Picante Valentina\n7 - Chiles Picantes Varios jalapeños,Habaneros, Arbol\n8 - ESC")
+let opcion = prompt("Ingrese que es lo que desea comprar: \n1 - Tortillas de Harina de Maíz\n2 - Tortillas de Harina de Trigo\n3 - Tortillas de Harina de Trigo para Burritos\n4 - Nachos de Harina de Maiz\n5 - Salsa Tabasco\n6 - Salsa Picante Valentina\n7 - Chiles Picantes Varios jalapeños,Habaneros, Arbol\n8 - Tequila\n9 - Mezcal\n -ESC")
 
 while(opcion != "ESC"){
     
@@ -81,12 +120,22 @@ while(opcion != "ESC"){
         CalculoStock(cantidadProductoG, productoG.stock, productoG.precio)
         productoG.restarStock(cantidadProductoG)
     }
+    else if(opcion == '8'){
+        let cantidadProductoH = prompt("ingrese que cantidad de " + productoH.nombre + " desea comprar:")
+        CalculoStock(cantidadProductoH, productoH.stock, productoH.precio)
+        productoH.restarStock(cantidadProductoH)
+    }
+    else if(opcion == '9'){
+        let cantidadProductoI = prompt("ingrese que cantidad de " + productoI.nombre + " desea comprar:")
+        CalculoStock(cantidadProductoI, productoI.stock, productoI.precio)
+        productoI.restarStock(cantidadProductoI)
+    }
     else{
         alert("No tenemos ese producto a la venta")
     }
-    opcion = prompt("Ingrese que es lo que desea comprar: \n1 - Tortillas de Harina de Maíz\n2 - Tortillas de Harina de Trigo\n3 - Tortillas de Harina de Trigo para Burritos\n4 - Nachos de Harina de Maiz\n5 - Salsa Tabasco\n6 - Salsa Picante Valentina\n7 - Chiles Picantes Varios jalapeños,Habaneros, Arbol\n8 - ESC")
+    opcion = prompt("Ingrese que es lo que desea comprar: \n1 - Tortillas de Harina de Maíz\n2 - Tortillas de Harina de Trigo\n3 - Tortillas de Harina de Trigo para Burritos\n4 - Nachos de Harina de Maiz\n5 - Salsa Tabasco\n6 - Salsa Picante Valentina\n7 - Chiles Picantes Varios jalapeños,Habaneros, Arbol\n8 - Tequila\n9 - Mezcal\n10 - ESC")
 }
 
 if(precioTotal != 0){
     alert("El precio total es: " + precioTotal)
-}
+}*/
